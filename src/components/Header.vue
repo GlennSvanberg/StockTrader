94b5b5cc2a1aafc44d5c884518c237ad09bf8ea5
Glenn Svanberg <signeratsvanberg@gmail.com>
2020-02-16 13:01:57 +0100

    Add project files

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+            </div>
+        
+        <div class="navbar-collapse">
+            <ul class="nav navbar-nav">
+                <router-link to='/portfolio' activeClass="active" tag="li"><a>Portfolio</a></router-link>
+                <router-link to='/stocks' activeClass="active" tag="li"><a>Stocks</a></router-link>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="">End Day</a></li>
+                <li class="dropdown">
+                    <a href="">Save & load</a>
+                </li>
+            </ul>
+        </div>   
+        </div>
+    </nav>
+</template>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This is the Stocks stock Component</h1>
+</template>
